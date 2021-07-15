6ab371e4635c0bd27d54e61bbaaad0587059f617
Vipulmahajan045 <vipulmahajan170140111045@gmail.com>
2021-07-15 16:19:53 +0530

    all done

--- kgtop.js
+++ kgtop.js
@@ -1,5 +1,15 @@
+  // weight converter kg to pounds
   function weightConverter(valNum){
       var valNum = valNum * 2.2046;
       var n = valNum.toFixed(0);
       document.getElementById("outputPounds").innerHTML = n;
+  }
+
+
+
+  //weight converter pounds to kg
+  function weightConverter1(valNum1){
+      var valNum1 = valNum1 / 2.2046;
+      var n1 = valNum1.toFixed(0);
+      document.getElementById("outputKilograms").innerHTML = n1;
   }
